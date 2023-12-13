b20d7174606a8c1b652e86b5a61184fcaec91172
root <root@DESKTOP-KNS4FK7>
2023-12-14 00:58:38 +0100

    added buttons that change scenes (right now background color changes)

--- src/test_gamestatemanager.py
+++ src/test_gamestatemanager.py
@@ -1,5 +1,6 @@
 import pygame
 from gamestatemanager import *
+from Button import Button
 pygame.init()
 
 # Set up display
@@ -16,6 +17,15 @@ level = BaseScene(screen, gameStateManager)
 
 states = {'start':start, 'level':level}
 
+def test(args):
+    gameStateManager.set_state('level')
+
+def test2(args):
+    gameStateManager.set_state('start')
+
+button = Button((100, 100), (200, 100), test, (0, 0, 0), (70, 70, 70), (200, 200, 200))
+button2 = Button((100, 300), (200, 100), test2, (0, 0, 0), (70, 70, 70), (200, 200, 200))
+
 while running:
     events = pygame.event.get()
     for event in events:
@@ -25,6 +35,14 @@ while running:
 
     states[gameStateManager.get_state()].render()
 
+    button.process_input(events, pygame.mouse)
+    button.update(pygame.mouse)
+    button.render(screen)   
+
+    button2.process_input(events, pygame.mouse, pygame.mouse.get_pos())
+    button2.update(pygame.mouse)
+    button2.render(screen)   
+    
     pygame.display.flip()
 
     clock.tick(60)

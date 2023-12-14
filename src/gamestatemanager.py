aa53a63071b91694116680a9e998b94c05395a2b
root <root@DESKTOP-KNS4FK7>
2023-12-14 01:24:45 +0100

    fixed spaces between defs

--- src/gamestatemanager.py
+++ src/gamestatemanager.py
@@ -6,17 +6,22 @@ class BaseScene:
         self.display = display
         self.gameStateManager = gameStateManager
         self.buttons = []
+        
     def add_button(self, button):
         self.buttons.append(button)
+        
     def process_input(self, events, pressed_keys):
         for button in self.buttons:
             button.process_input(events, pygame.mouse)
+            
     def update(self, mouse=pygame.mouse):
         for button in self.buttons:
             button.update(pygame.mouse)
+            
     def render_base_ui(self, screen):
         for button in self.buttons:
             button.render(screen)   
+            
     def render(self, screen):
         self.display.fill('blue')
         self.render_base_ui(screen)
@@ -24,6 +29,7 @@ class BaseScene:
 class Start(BaseScene):
     def __init__(self, display, gameStateManager):
         BaseScene.__init__(self,display=display,gameStateManager=gameStateManager)
+        
     def render(self, screen):
         self.display.fill('red')
         self.render_base_ui(screen)
@@ -31,7 +37,9 @@ class Start(BaseScene):
 class GameStateManager:
     def __init__(self, currentState):
         self.currentState=currentState
+        
     def get_state(self):
         return self.currentState
+    
     def set_state(self, state):
         self.currentState=state

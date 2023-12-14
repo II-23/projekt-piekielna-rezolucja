d60af935088c98cc6446c1748e75de96316047de
root <root@DESKTOP-KNS4FK7>
2023-12-14 01:12:04 +0100

    added ability for buttons to be added to scenes, now each screen shows only its buttons

--- src/gamestatemanager.py
+++ src/gamestatemanager.py
@@ -1,19 +1,32 @@
+from Button import Button
+import pygame
+
 class BaseScene:
     def __init__(self, display, gameStateManager):
         self.display = display
         self.gameStateManager = gameStateManager
+        self.buttons = []
+    def add_button(self, button):
+        self.buttons.append(button)
     def process_input(self, events, pressed_keys):
-        pass
-    def update(self):
-        pass
-    def render(self):
+        for button in self.buttons:
+            button.process_input(events, pygame.mouse)
+    def update(self, mouse=pygame.mouse):
+        for button in self.buttons:
+            button.update(pygame.mouse)
+    def render_base_ui(self, screen):
+        for button in self.buttons:
+            button.render(screen)   
+    def render(self, screen):
         self.display.fill('blue')
+        self.render_base_ui(screen)
 
 class Start(BaseScene):
     def __init__(self, display, gameStateManager):
         BaseScene.__init__(self,display=display,gameStateManager=gameStateManager)
-    def render(self):
+    def render(self, screen):
         self.display.fill('red')
+        self.render_base_ui(screen)
         
 class GameStateManager:
     def __init__(self, currentState):

32a6dbd884253bf5796eaa2bfef1981361cf8c04
matkat1 <72603220+matkat1@users.noreply.github.com>
2024-01-07 21:06:05 +0100

    added resolution
    button now checks if there are two formulas selected, if resolving them makes sense, and if both are true it adds a new formula to set

--- src/char.py
+++ src/char.py
@@ -59,16 +59,13 @@ class Formula(pygame.sprite.Sprite):
         self.y=pos[1]
         self.max_width=width
         self.tab=[]
-        #
-        #self.surface=pygame.Surface((200,200))
         
         loaded_bar = pygame.image.load("./assets/slider_bar.png").convert_alpha()
         self.surface=pygame.transform.scale(loaded_bar, (self.width*12, self.height)).convert_alpha()
         
-
+        self.content=list
         self.formula_rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
-        #
         counter=0
         for i in range(len(list)):
             if list[i]==1:
@@ -96,14 +93,16 @@ class Formula(pygame.sprite.Sprite):
         return self.x<pos[0]<self.x+self.width*12 and self.y<pos[1]<self.y+self.height
     def update(self, mouse=pygame.mouse):
         mouse_pos = (mouse.get_pos()[0], mouse.get_pos()[1])
-        if self.state==0 and self.cursor_over_formula(mouse_pos)==True and self.clickable:
-            for element in self.tab:
-                element.state=1
-            self.state=1
         if self.state==1 and self.cursor_over_formula(mouse_pos)==False and self.clickable:
             for element in self.tab:
                 element.state=0
             self.state=0
+
+        if self.state==0 and self.cursor_over_formula(mouse_pos)==True and self.clickable:
+            for element in self.tab:
+                element.state=1
+            self.state=1
+        
     def process_input(self, events, mouse, *args):
         pos=mouse.get_pos()
         for event in events:
@@ -121,12 +120,10 @@ class Formula(pygame.sprite.Sprite):
 class Set_of_formulas(pygame.sprite.Sprite):
     def __init__(self, size, pos, list):
         self.selected=[Formula((25,25), (500,100), [1,1,0], 500, False),Formula((25,25), (800,100), [1,0,2], 500, True)]
-        #
         self.button=Button((500,500), (100,100), self.button_clicked, "red", "green", "blue")
         loaded_bar = pygame.image.load("./assets/slider_bar.png").convert_alpha()
         self.surface=pygame.transform.scale(loaded_bar, size).convert_alpha()
         self.set_rect = pygame.Rect(pos[0], pos[1], 500, 500)
-        #
         self.selected_index=[1,1]
         self.width=size[0]
         self.height=size[1]
@@ -137,7 +134,45 @@ class Set_of_formulas(pygame.sprite.Sprite):
         for i in range(len(list)):
            self.tab.append(Formula((25,25), (self.x, self.y+i*25), list[i][1], self.width, True))
     def button_clicked(self, *args):
-        self.tab.append(Formula((25,25), (self.x, self.y+len(self.tab)*25), [1,1,-1], self.width, True))
+        if self.selected[0].tab==[] or self.selected[1].tab==[]:
+            print("głupota")
+        else:
+            q1=self.selected[0].content.copy()
+            q2=self.selected[1].content.copy()
+            q3=[]
+            x=len(q1)
+            check=-1
+            for i in range(x):
+                if (q1[i]==1 and q2[i]==-1) or (q1[i]==-1 and q2[i]==1):
+                    check=i
+            if check==-1:
+                print("głupota")
+            else:
+                for i in range(x):
+                    if i==check:
+                        q3.append(0)
+                    elif q1[i]==0 and q2[i]==0:
+                        q3.append(0)
+                    elif q1[i]==1 or q2[i]==1:
+                        q3.append(1)
+                    elif q1[i]==-1 or q2[i]==-1:
+                        q3.append(-1)
+                check=0
+                for x in q3:
+                    if x!=0:
+                        check=1
+                if check==1:
+                    self.tab.append(Formula((25,25), (self.x, self.y+len(self.tab)*25), q3, self.width, True))
+                    self.selected_index[0]=-1
+                    self.selected[0].tab=[]
+                    self.selected[0].content=[]
+                    self.selected_index[1]=-1
+                    self.selected[1].tab=[]
+                    self.selected[1].content=[]
+                    for x in self.tab:
+                        x.state=1
+                else:
+                    print("znaleziono sprzeczność")
     def render(self, screen):
         self.get_surface().fill((0,0,0,0))
         for x in range(len(self.tab)):
@@ -148,11 +183,13 @@ class Set_of_formulas(pygame.sprite.Sprite):
             if self.tab[self.selected_index[0]].state!=3:
                 self.selected_index[0]=-1
                 self.selected[0].tab=[]
+                self.selected[0].content=[]
                 
         if self.selected_index[1]!=-1:
             if self.tab[self.selected_index[1]].state!=4:
                 self.selected_index[1]=-1
                 self.selected[1].tab=[]
+                self.selected[1].content=[]
         for x in range(len(self.tab)):
             
             if self.tab[x].state==2:
@@ -160,10 +197,12 @@ class Set_of_formulas(pygame.sprite.Sprite):
                     self.tab[x].state=3
                     self.selected_index[0]=x
                     self.selected[0].tab=self.tab[x].tab
+                    self.selected[0].content=self.tab[x].content
                 elif self.selected_index[1]==-1:
                     self.tab[x].state=4
                     self.selected_index[1]=x
                     self.selected[1].tab=self.tab[x].tab
+                    self.selected[1].content=self.tab[x].content
                 else:
                      self.tab[x].state=1
         for x in self.tab:

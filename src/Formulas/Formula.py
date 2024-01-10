791c06bb47f10bf797e55ddedd94d0ee0e661360
szaranczuk <igor.hanczaruk@gmail.com>
2024-01-10 21:17:51 +0100

    Further refactorisation, added Symbol_Type enum and changed some things

--- src/Formulas/Formula.py
+++ src/Formulas/Formula.py
@@ -1,36 +1,32 @@
 import pygame
 from Utils.Button import Button
 from Config.definitnios import ASSETS_DIR
+from enum import Enum
+import os
 
+non_hover_symbol_list = [ASSETS_DIR + "/variables/non_hover/" + variable_asset for variable_asset in os.listdir(ASSETS_DIR + "/variables/non_hover")]
+
+class Symbol_Type(Enum):
+    ALTERNATIVE = 1
+    NEGATION = 2
+    VARIABLE = 3
+
+#If type is Symbol_Type.VARIABLE, then kwarg variable_index must be specified
 class Symbol(pygame.sprite.Sprite):
-    def __init__(self, size, num, pos):
+    def __init__(self, size, type, pos, **kwargs):
         self.state=0
         self.width = size[0]
         self.height = size[1]
         self.x=pos[0]
         self.y=pos[1]
         #assigning image to symbol according to num value. 0 is alternate, -1 is negate. 2nd row is a placeholder for colored icons(hovered)
-        match num:
-            case 1:
-                self.symbol = [pygame.image.load(ASSETS_DIR + "/one.png").convert_alpha(), pygame.image.load(ASSETS_DIR + "/minus-sign.png").convert_alpha()]
-            case 2:
-                self.symbol = [pygame.image.load(ASSETS_DIR + "/2.png").convert_alpha(), pygame.image.load(ASSETS_DIR + "/minus-sign.png").convert_alpha()]
-            case 3:
-                self.symbol = [pygame.image.load(ASSETS_DIR + "/three.png").convert_alpha(), pygame.image.load(ASSETS_DIR + "/minus-sign.png").convert_alpha()]
-            case 4:
-                self.symbol = [pygame.image.load(ASSETS_DIR + "/4.png").convert_alpha(), pygame.image.load(ASSETS_DIR + "/minus-sign.png").convert_alpha()]
-            case 5:
-                self.symbol = [pygame.image.load(ASSETS_DIR + "/five.png").convert_alpha(), pygame.image.load(ASSETS_DIR + "/minus-sign.png").convert_alpha()]
-            case 6:
-                self.symbol = [pygame.image.load(ASSETS_DIR + "/6.png").convert_alpha(), pygame.image.load(ASSETS_DIR + "/minus-sign.png").convert_alpha()]
-            case 7:
-                self.symbol = [pygame.image.load(ASSETS_DIR + "/seven.png").convert_alpha(), pygame.image.load(ASSETS_DIR + "/minus-sign.png").convert_alpha()]
-            case 8:
-                self.symbol = [pygame.image.load(ASSETS_DIR + "/eight.png").convert_alpha(), pygame.image.load(ASSETS_DIR + "/minus-sign.png").convert_alpha()]
-            case 0:
+        match type:
+            case Symbol_Type.ALTERNATIVE:
                 self.symbol = [pygame.image.load(ASSETS_DIR + "/alternate.png").convert_alpha(), pygame.image.load(ASSETS_DIR + "/minus-sign.png").convert_alpha()]
-            case -1:
+            case Symbol_Type.NEGATION:
                 self.symbol = [pygame.image.load(ASSETS_DIR + "/minus-sign.png").convert_alpha(), pygame.image.load(ASSETS_DIR + "/minus-sign.png").convert_alpha()]
+            case Symbol_Type.VARIABLE:
+                self.symbol = [pygame.image.load(non_hover_symbol_list[kwargs['variable_index']]).convert_alpha(), pygame.image.load(ASSETS_DIR + "/minus-sign.png").convert_alpha()]
         
         self.surface = pygame.transform.scale(self.symbol[0], (self.width, self.height))
         self.symbol_rect = pygame.Rect(self.x, self.y, self.width, self.height)
@@ -72,17 +68,17 @@ class Formula(pygame.sprite.Sprite):
         for i in range(len(list_of_formulas)):
             if list_of_formulas[i]==1:
                 if len(self.tab)>0:
-                    self.tab.append(Symbol((self.width, self.height), 0, (pos[0]+counter*self.width, pos[1])))
+                    self.tab.append(Symbol((self.width, self.height), Symbol_Type.ALTERNATIVE, (pos[0]+counter*self.width, pos[1])))
                     counter+=1
-                self.tab.append(Symbol((self.width, self.height), i+1, (pos[0]+counter*self.width, pos[1])))
+                self.tab.append(Symbol((self.width, self.height), Symbol_Type.VARIABLE, (pos[0]+counter*self.width, pos[1]), variable_index=i))
                 counter=counter+1
             if list_of_formulas[i]==-1:
                 if len(self.tab)>0:
-                    self.tab.append(Symbol((self.width, self.height), 0, (pos[0]+counter*self.width, pos[1])))
+                    self.tab.append(Symbol((self.width, self.height), Symbol_Type.ALTERNATIVE, (pos[0]+counter*self.width, pos[1])))
                     counter+=1
-                self.tab.append(Symbol((self.width, self.height), -1, (pos[0]+counter*self.width, pos[1])))
+                self.tab.append(Symbol((self.width, self.height), Symbol_Type.NEGATION, (pos[0]+counter*self.width, pos[1])))
                 counter=counter+1
-                self.tab.append(Symbol((self.width, self.height), i+1, (pos[0]+counter*self.width, pos[1])))
+                self.tab.append(Symbol((self.width, self.height), Symbol_Type.VARIABLE, (pos[0]+counter*self.width, pos[1]), variable_index=i))
                 counter=counter+1
     def render(self, screen):
         #fill with transparent and blit all of the symbols stored in "tab"

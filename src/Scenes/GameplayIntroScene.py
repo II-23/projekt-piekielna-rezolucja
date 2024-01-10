b84e2f6b3ffcd83c67bce57d61b3fc09ede317a4
root <root@DESKTOP-KNS4FK7>
2024-01-10 23:34:30 +0100

    added actors for npc during cutscenes

--- src/Scenes/GameplayIntroScene.py
+++ src/Scenes/GameplayIntroScene.py
@@ -1,12 +1,27 @@
 from Scenes.BaseScene import BaseScene, setup_button, Button
 from Slider import Slider_Bar
+from enum import Enum
 import pygame
 
+class Side(Enum):
+    LEFT = -1
+    RIGHT = 1
+
 RESOLUTION = (1280, 720)
+
+class Actor:
+    '''This is a class for the npcs that talk during cutscenes'''
+    def __init__(self):
+        self.dialog = []
+        
 
 class GameplayIntroScene(BaseScene):
     def __init__(self, display, gameStateManager, background_color=(255, 255, 255)):
         BaseScene.__init__(self, display=display, gameStateManager=gameStateManager, background_color=background_color)
+        
+        self.talking_side = Side.LEFT # to remember which side is currently talking
+        self.left_npc = Actor()
+        self.right_npc = Actor()
         
         intro_text = 'Jak to jest być skrybą, dobrze? \
         A, wie pan, moim zdaniem to nie ma tak, że dobrze, albo że niedobrze.\
@@ -22,7 +37,7 @@ class GameplayIntroScene(BaseScene):
         
         col = (255, 135, 135)
         tw_size = (800, 250)
-        self.text_window = Button((RESOLUTION[0]/2-tw_size[0]/2, 460), tw_size, None, col, col, col)
+        self.text_window = Button((RESOLUTION[0]/2-tw_size[0]/2, 460), tw_size, None, col, col, (255,160,160))
         self.text_window.init_text(font=None, text_size=32, color=(42, 62, 115), text=intro_text)
         def foo(args):
             self.text_window.text_next_page()

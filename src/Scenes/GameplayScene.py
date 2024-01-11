e35425e5b289339b4c4a86539bdac87b592e1f66
szaranczuk <igor.hanczaruk@gmail.com>
2024-01-11 15:30:56 +0100

    Replaced old button with new one

--- src/Scenes/GameplayScene.py
+++ src/Scenes/GameplayScene.py
@@ -5,6 +5,7 @@ from Formulas.Formula import Symbol, Formula
 from Formulas.FormulaSet import Set_of_formulas
 from Formulas.FormulaGenerator import *
 from Config.definitnios import ASSETS_DIR
+from Utils.ResolutionButton import ResolutionButton
 import pygame
 
 class GameplayScene(BaseScene):
@@ -42,8 +43,10 @@ class GameplayScene(BaseScene):
         #
 
         formula_set=Set_of_formulas((500,500), (500,150), formulas)
+
         self.add_ui_element(formula_set)
         self.add_ui_element(formula_set.selected[0])
         self.add_ui_element(formula_set.selected[1])
         self.add_ui_element(formula_set.button)
+
         #    
--- src/Utils/ResolutionButton.py
+++ src/Utils/ResolutionButton.py
@@ -7,18 +7,23 @@ from Config.definitnios import ASSETS_DIR
 COLOR_PLACEHOLDER = (0,0,0) #Used bcs I wanted to inherit from Button class, but its color feature is useless for me
 
 class ResolutionButton(Button, pygame.sprite.Sprite):
-    def __init__(self, position, size, on_click_event):
-        Button.__init__(self, position, size, on_click_event, COLOR_PLACEHOLDER, COLOR_PLACEHOLDER, COLOR_PLACEHOLDER)
+    def __init__(self, position, on_click_event):
         FRAME_PATH = os.path.join(ASSETS_DIR, "resolution_button_frames")
         FRAME_UPDATE_RATE = 0.05
         self.gif_player = GifPlayer(FRAME_PATH, FRAME_UPDATE_RATE)
         self.is_visible = False
+        self.size = self.gif_player.get_surface().get_size()
+        Button.__init__(self, position, self.size, on_click_event, COLOR_PLACEHOLDER, COLOR_PLACEHOLDER, COLOR_PLACEHOLDER)
     
     def make_visible(self):
         self.is_visible = True
 
     def make_not_visible(self):
         self.is_visible = False
+
+    def update(self, mouse):
+        Button.update(self, mouse)
+        self.gif_player.update(mouse)
 
     def render(self, screen):
         if (self.is_visible):
@@ -29,3 +34,9 @@ class ResolutionButton(Button, pygame.sprite.Sprite):
     
     def get_rect(self):
         return pygame.Rect(*self.position, *self.size)
+
+    def get_height(self):
+        return self.size[1]
+
+    def get_width(self):
+        return self.size[0]

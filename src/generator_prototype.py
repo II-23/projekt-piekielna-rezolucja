336a86ff33790d0633a765cb2ebf8a8e0d559c8b
domgwarda <domi.gwarda@gmail.com>
2024-01-11 19:00:17 +0100

    Updated formula_generator

--- src/generator_prototype.py
+++ src/generator_prototype.py
@@ -8,44 +8,41 @@ def generate_formula(letters):
     while True: 
         x, y = [choice(letters), randint(0,1)], [choice(letters), randint(0,1)] #choice first variable and its sign, choice seciund -||-
         if x[0]==y[0]: continue
-        x = Not(Variable(x[0])) if x[1]==0 else Variable(x[0])
-        y = Not(Variable(y[0])) if y[1]==0 else Variable(y[0])
+        x_var = Not(Variable(x[0])) if x[1]==0 else Variable(x[0])
+        y_var = Not(Variable(y[0])) if y[1]==0 else Variable(y[0])
         chance = randint(0,100)
-        if chance>=70:
+        if chance>=0:
             z = [choice(letters), randint(0,1)]
-            z = Not(Variable(z[0])) if z[1]==0 else Variable(z[0])
-            formula = Or(Or(x,y),z)
+            if z[0]==x[0] or z[0]==y[0]: continue
+            z_var = Not(Variable(z[0])) if z[1]==0 else Variable(z[0])
+            formula = Or(Or(x_var,y_var),z_var)
         else:
-            formula = Or(x,y)
+            formula = Or(x_var,y_var)
         if formula.tautology() or Not(formula).tautology():
                 continue
         return formula
 
-class Set_formulas_n:
-    def __init__(self, size, letters):
-        self.size=size
-        self.letters=letters
-        self.set = []
+# class Set_formulas_n:
+#     def __init__(self, size, letters):
+#         self.size=size
+#         self.letters=letters
+#         self.set = []
 
-    def clear(self):
-        self.set=[]
+#     def clear(self):
+#         self.set=[]
     
-    def clear_one(self, index):
-        self.set.pop(index)
+#     def clear_one(self, index):
+#         self.set.pop(index)
 
-    def fill(self):
-        for _ in range(self.size):
-            while True:
-                formula = generate_formula(self.letters)
-                for f in self.set:
-                    if f==formula: continue
-                self.set.append(formula)
-                break
+#     def fill(self):
+#         for _ in range(self.size):
+#             while True:
+#                 formula = generate_formula(self.letters)
+#                 self.set.append(formula)
+#                 break
 
-    def chceck(self):
-        ...
-    
-
+#     def chceck(self):
+#         ...
             
 # size = 5
 # x = Set_formulas_n(size,letters)

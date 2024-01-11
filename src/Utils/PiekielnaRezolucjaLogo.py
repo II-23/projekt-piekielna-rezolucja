66080a6ae838db725a13e4997719e1f1a843f493
szaranczuk <igor.hanczaruk@gmail.com>
2024-01-11 12:19:21 +0100

    Extracted gif player from main menu logo

--- src/Utils/PiekielnaRezolucjaLogo.py
+++ src/Utils/PiekielnaRezolucjaLogo.py
@@ -1,28 +1,22 @@
 import pygame
 import os
+from Utils.GifPlayer import GifPlayer
 from Config.definitnios import ASSETS_DIR
-import time
 
-class PiekielnaRezolucjaLogo(pygame.sprite.Sprite):
+class PiekielnaRezolucjaLogo:
     def __init__(self, pos):
         x, y = pos
-        self.pos_rect = pygame.Rect(x,y,0,0)
-        self.piekielna_rezolucja_frames = [pygame.image.load(os.path.join(ASSETS_DIR, "piekielna_rezolucja_gif", frame)) for frame in sorted(os.listdir(os.path.join(ASSETS_DIR, "piekielna_rezolucja_gif")))]
-        self.previous_step_time = time.time()
-        self.FRAME_UPDATE_RATE = 0.05
-        self.current_frame = 0
+        self.pos_rect = pygame.Rect(x, y, 0, 0)
+        self.gif_path = os.path.join(ASSETS_DIR, "piekielna_rezolucja_gif")
+        self.frame_update_rate = 0.05
+        self.logo_gif_player = GifPlayer(self.gif_path, self.frame_update_rate)
+    def get_surface(self):
+        return self.logo_gif_player.get_surface()
+    def get_rect(self):
+        return self.pos_rect
     def process_input(self, events, mouse, *args):
         pass
     def update(self, mouse):
-        current_time = time.time()
-        time_elapsed = current_time - self.previous_step_time
-        if (time_elapsed >= self.FRAME_UPDATE_RATE):
-            self.current_frame = (self.current_frame + 1) % len(self.piekielna_rezolucja_frames)
-            self.previous_step_time = current_time
+        self.logo_gif_player.update(mouse)
     def render(self, screen):
         pass
-    def get_surface(self):
-        print(self.current_frame)
-        return self.piekielna_rezolucja_frames[self.current_frame]
-    def get_rect(self):
-        return self.pos_rect

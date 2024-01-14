d1394e47122f396170e0ef17620e159a97d9ed31
o0something <just44d4@gmail.com>
2024-01-14 22:12:23 +0100

    Update volume_slider.py
    added get_value function

--- src/volume_slider.py
+++ src/volume_slider.py
@@ -15,19 +15,37 @@ class Volume_slider:
         self.initaial_val=(self.slider_right_pos-self.slider_left_pos)*initaial_val
 
         self.bar_rect=pygame.Rect(self.slider_left_pos,self.slider_top_pos,self.size[0],self.size[1])
-        self.button_rect=pygame.Rect(self.slider_left_pos+self.initaial_val-3,self.slider_top_pos,15,self.size[1])
+        self.button_rect=pygame.Rect(self.slider_left_pos+self.initaial_val-6,self.slider_top_pos,12,self.size[1])
+
+    def move_slider(self, mouse_pos):
+        new_x = mouse_pos[0]
+        # borders
+        # new_x = max(self.slider_left_pos + self.button_rect.width , new_x)
+        # new_x = min(self.slider_right_pos - self.button_rect.width , new_x)
+        self.button_rect.centerx = new_x
 
     def render(self,screen):
         pygame.draw.rect(screen,GRAY_COLOR,self.bar_rect)
         pygame.draw.rect(screen,"lightblue",self.button_rect)
 
+    def get_value(self):
+        val_range=self.slider_right_pos-self.slider_left_pos -1
+        button_val=self.button_rect.centerx-self.slider_left_pos
+        result_val=(button_val/val_range)*(self.max_val-self.min_val)+self.min_val
+        return result_val
+
 # test runs
 class tests:
     def __init__(self, screen) -> None:
         self.screen = screen
-        self.sliders=[Volume_slider((100,3),(100,10),0.5,0,100)]
+        self.sliders=[Volume_slider((100,3),(100,12),0.5,0,100)]
     def run(self):
+        mouse_pos=pygame.mouse.get_pos()
+        mouse=pygame.mouse.get_pressed()
         for i in self.sliders:
+            if i.bar_rect.collidepoint(mouse_pos) and mouse[0]:
+                i.move_slider(mouse_pos)
+            print(i.get_value())
             i.render(self.screen)
 
 pygame.init()

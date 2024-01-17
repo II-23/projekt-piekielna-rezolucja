ebc541924555d18dcfc4fc2b82cda25069b92c10
domgwarda <domi.gwarda@gmail.com>
2024-01-17 17:41:28 +0100

    derive_formulas update: variable and 3elements formula

--- src/generator_prototype.py
+++ src/generator_prototype.py
@@ -4,24 +4,6 @@ from random import choice, randint
 letters = ['p','q','r','s','t','u','w']
 # letters = ['p','q','r']
 
-# def generate_formula(letters):
-#     while True: 
-#         x, y = [choice(letters), randint(0,1)], [choice(letters), randint(0,1)] #choice first variable and its sign, choice seciund -||-
-#         if x[0]==y[0]: continue
-#         x_var = Not(Variable(x[0])) if x[1]==0 else Variable(x[0])
-#         y_var = Not(Variable(y[0])) if y[1]==0 else Variable(y[0])
-#         chance = randint(0,100)
-#         if chance>=0:
-#             z = [choice(letters), randint(0,1)]
-#             if z[0]==x[0] or z[0]==y[0]: continue
-#             z_var = Not(Variable(z[0])) if z[1]==0 else Variable(z[0])
-#             formula = Or(Or(x_var,y_var),z_var)
-#         else:
-#             formula = Or(x_var,y_var)
-#         if formula.tautology() or Not(formula).tautology():
-#                 continue
-#         return formula
-    
 def generate_variable(letters):
     return Variable(choice(letters))
 
@@ -44,17 +26,34 @@ def derive_formulas(letters, formula, letter=None):
     if len(formula.used_variables()) == 2:
             f1 = formula.formula1
             f2 = formula.formula2
-
             if not letter:
                 letter = choice(letters)
                 while letter in formula.used_variables():
                     letter = choice(letters)
+            new = Variable(letter)
+            type_ = randint(1,3)
 
-            type_ = randint(2,3)
+            if type_ == 1:
+                chance = randint(0,1)
+                if chance:
+                    chance = randint(0,1)
+                    if chance:
+                        x = new
+                        y = Or(Or(f1, f2), Not(new))
+                    else:
+                        x = Not(new)
+                        y = Or(Or(f2, f1), new)
+                else:
+                    if chance:
+                        x = new
+                        y = Or(Or(Not(new), f2), f1)
+                    else:
+                        x = Not(new)
+                        y = Or(Or(f2, new), f1)
 
             if type_ == 2:
-                x = Variable(letter)
-                y = Not(Variable(letter))
+                x = new
+                y = Not(new)
                 order = randint(1,4)
                 if order==1: return (Or(x, f1), Or(y, f2))
                 if order==2: return (Or(f1, x), Or(f2, y))
@@ -63,7 +62,6 @@ def derive_formulas(letters, formula, letter=None):
 
             if type_ == 3:
                 chance = randint(0,2)
-                new = Variable(letter)
                 if chance==0:
                     chance = randint(0,1)
                     if chance:
@@ -91,14 +89,14 @@ def derive_formulas(letters, formula, letter=None):
                         x = Or(Or(f1, Not(new)), f2)
                         y = Or(choice((f1,f2)), new)
 
-                return (x,y)
+            return (x,y)
                          
     if len(formula.used_variables()) == 3:
             if len(formula.formula1.used_variables())==1: f1, f2, f3 = formula.formula1, formula.formula2.formula1, formula.formula2.formula2
             else: f1, f2, f3 = formula.formula1.formula1, formula.formula1.formula2, formula.formula2
-            return (f1,f2,f3)
+            
 
-x=derive_formulas(letters, Or(Variable('p'), Variable('r')))
+x=derive_formulas(letters, Or(Not(Variable('q')), Variable('r')))
 for i in x:
       print(i)
 # print(x)

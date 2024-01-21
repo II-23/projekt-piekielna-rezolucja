37148aec658209f7bf0a5b3b41ea7c53dc9e77f9
Krzychuo <krzychuo93@gmail.com>
2024-01-21 13:46:02 +0100

    Added handling for pygame.mixer not being initialized

--- src/soundtrackmanager.py
+++ src/soundtrackmanager.py
@@ -7,9 +7,17 @@ class SoundtrackManager():
     def __init__(self):
         self.currentMusic = None
 
+    def mixerCheck(func):
+        def wrapper(*args, **kwargs):
+            if not pygame.mixer.get_init():
+                return
+            func(*args, **kwargs)
+        return wrapper
+
     def getPath(self, fileName):
         return os.path.join(ASSETS_DIR,"soundtrack",fileName)
 
+    @mixerCheck
     def playMusic(self, fileName, *args, **kwargs):
         musicPath = self.getPath(fileName)
         try:
@@ -19,18 +27,21 @@ class SoundtrackManager():
         except Exception as e:
             print(f"Wasn't able to load/play music file {fileName}: {e}")
 
+    @mixerCheck
     def pauseMusic(self):
         try:
             pygame.mixer.music.pause()
         except Exception as e:
             print(e)
 
+    @mixerCheck
     def unpauseMusic(self):
         try:
             pygame.mixer.music.unpause()
         except Exception as e:
             print(e)
 
+    @mixerCheck
     def stopMusic(self, fileName):
         musicPath = self.getPath(fileName)
         try:

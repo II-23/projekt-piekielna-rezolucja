9c4f6eb484aa314fdbed748935f64f33ebafc1e4
szaranczuk <igor.hanczaruk@gmail.com>
2024-01-21 11:07:12 +0100

    Changed old fonts in gameplay scene to new ones

--- src/Formulas/Formula.py
+++ src/Formulas/Formula.py
@@ -4,7 +4,8 @@ from Config.definitnios import ASSETS_DIR
 from enum import Enum
 import os
 
-non_hover_symbol_list = [ASSETS_DIR + "/variables/non_hover/" + variable_asset for variable_asset in os.listdir(ASSETS_DIR + "/variables/non_hover")]
+non_hover_symbol_list = sorted([os.path.join(ASSETS_DIR,"variables","non_hover",variable_asset) for variable_asset in os.listdir(os.path.join(ASSETS_DIR,"variables","non_hover"))])
+hover_symbol_list = sorted([os.path.join(ASSETS_DIR,"variables","hover",variable_asset) for variable_asset in os.listdir(os.path.join(ASSETS_DIR,"variables","hover"))])
 
 class Symbol_Type(Enum):
     ALTERNATIVE = 1
@@ -33,7 +34,7 @@ class Symbol(pygame.sprite.Sprite):
             case Symbol_Type.NEGATION:
                 self.symbol = [pygame.image.load(ASSETS_DIR + "/not.png").convert_alpha(), pygame.image.load(ASSETS_DIR + "/not_h.png").convert_alpha()]
             case Symbol_Type.VARIABLE:
-                self.symbol = [pygame.image.load(non_hover_symbol_list[kwargs['variable_index']]).convert_alpha(), pygame.image.load(ASSETS_DIR + "/minus-sign.png").convert_alpha()]
+                self.symbol = [pygame.image.load(non_hover_symbol_list[kwargs['variable_index']]).convert_alpha(), pygame.image.load(hover_symbol_list[kwargs['variable_index']]).convert_alpha()]
         
         self.surface = pygame.transform.scale(self.symbol[0], (self.width, self.height))
         self.symbol_rect = pygame.Rect(self.x, self.y, self.width, self.height)

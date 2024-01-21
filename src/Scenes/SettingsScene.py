ee99c9f979210f3662caeb96014d775591e2935a
root <root@adziw>
2024-01-21 12:08:07 +0100

    added volume slider to settings

--- src/Scenes/SettingsScene.py
+++ src/Scenes/SettingsScene.py
@@ -17,10 +17,15 @@ class SettingsScene(BaseScene):
         # backgorund for settings
         piwo_img = pygame.image.load(ASSETS_DIR + "/piwo.png")
         self.add_background_image(piwo_img)
-
-        self.slider = Volume_slider((200, 250), (200, 12), 0.5,0,100,'red','grey',50)
+        # positions for easy placing of new things
+        #settings_pos = [i]
+        help_text_color = (166, 54, 54)
+        self.slider_text = Button((340, 180), (600, 50), None, help_text_color, help_text_color, help_text_color)
+        self.slider_text.init_text(None, 36, (0,0,0), "Volume: ")
+        self.slider = Volume_slider((650, 180+25-10), (200, 20), 0.5,0,100,'red','grey',50)
+        self.add_ui_element(self.slider_text)
         self.add_ui_element(self.slider)
 
-        self.back_to_menu_button = setup_button(gameStateManager, 'start', (800, 600))
+        self.back_to_menu_button = setup_button(gameStateManager, 'start', (1060, 600))
         self.back_to_menu_button.init_text(text="return to menu")
         self.add_ui_element(self.back_to_menu_button)

0fc34a02ffb8ed750738bbf99f1e074c7eecf151
Igor Hańczaruk <igor.hanczaruk@gmail.com>
2024-01-22 17:39:04 +0100

    Revert "Added different levels of difficulty to generator. "

--- src/Formulas/FormulaGenerator.py
+++ src/Formulas/FormulaGenerator.py
@@ -1,18 +1,12 @@
 from random import *
 
-CHANCE_FOR_SATISFIABLE = 30 # as a percentage
+CHANCE_FOR_SATISFIABLE = 100 # as a percentage
+
+max_variable_number= 5
+formulas_number=5 # has to be at least 2
+max_len=4 # has to be lower than max_variable_number
 
 """
-USAGE:
-To generate a set of fomulas create an object Generator, which takes
-"""
-
-
-"""
-max_variable_number - how many distinct variables are there
-max_len - maximum length a single formula can have
-formulas_number - how many formulas are there in a set
-
 class Formula
     stores:
         - [int] length - the number of set variables in the Formula
@@ -33,10 +27,9 @@ class Set_Of_Formula
         - [list] valuation - example of correct valuation; valuation[i] == 1 - i'th variable is true, valuation[i] == -1 - i'th variable is false, valuation[i] == 0 - value of i'th variable can be both true and false
 
     does:
-        - __init__ - creates an empty set with sets max_variable_number, max_len and number of formulas according to difficulty.
-        Draws whether a set will be satisfiable or not. Then calls fill
-        - fill - generates formulas with full type tree - medium and hard levels
-        - fill_simple - generates easy contradictory sets with tree-like resolution structure
+        - __init__ - creates an empty set with size = formulas_number, satisfiable = False, formulas = list of size = formulas_number storing empty class Formula objects whose sizes are set to max_variable_number], valuation - [0,0,...,0]
+        - clear - sets all attributes of a set to initial values as in __init__
+        - fill - according to CHANCE_OF_SATISFIABLE generates either satisfiable or contradictory sets of formulas
 """
 
 
@@ -61,75 +54,30 @@ class Formula:
 
 
 class Generator:
-    def __init__(self, difficulty):
-        satisfiable = (randint(0, 100) <= CHANCE_FOR_SATISFIABLE)
-        print(satisfiable)
-        if difficulty == 1:
-            max_variable_number = 4
-            formulas_number = 4
-            max_len = 3
-        elif difficulty == 2:
-            max_variable_number = 5
-            formulas_number = 6
-            max_len = 4
-        elif difficulty == 3:
-            max_variable_number = 6
-            formulas_number = 7
-            max_len = 5
-        else:
-            max_variable_number = 7
-            formulas_number = 8
-            max_len = 6
-        
+    def __init__(self, size, max_variable_number):
         self.formulas = [] # stores the list of formulas
-        self.size = formulas_number # how many formulas are there in the set
-        self.satisfiable = satisfiable # True = the set is satisfiable, False = the set is NOT satisfiable
+        self.size = size # how many formulas are there in the set
+        self.satisfiable = True # True = the set is satisfiable, False = the set is NOT satisfiable
         self.valuation = [] # every element in the list is linked to a variable; 1 = the variable has to have value true; -1 = the variable has to have value false; 0 = the variable can have any value; if self.satisfiable is False then self.valuation is all 0's
         for i in range(self.size):
             self.formulas.append(Formula(max_variable_number))
         for i in range(max_variable_number):
             self.valuation.append(0)
-        if difficulty == 1:
-            self.fill_simple(satisfiable, max_len, max_variable_number)
-        else:
-            self.fill(satisfiable, max_len, max_variable_number)
 
     def clear(self):
         for formula in self.formulas:
-            formula.clear()
+            formula.clear
         self.satisfiable = False
         for i in range(len(self.valuation)):
             self.valuation[i] = 0
-
-    def fill_simple(self, satisfiable, max_len, max_variable_number):
-        if satisfiable: 
-            self.fill(satisfiable, max_len, max_variable_number)
-        else:
-            vars = [i for i in range(max_variable_number)]
-            formulas = []
-            formulas.append(Formula(max_variable_number))
-            while len(vars) > 0:
-                if len(formulas) == self.size:
-                    break
-                modified_formula = choice(formulas)
-                if modified_formula.length == max_len:
-                    continue
-                var = choice(vars)
-                vars.remove(var)
-                modified_formula_new = modified_formula.copy()
-                modified_formula.variables[var] = choice([-1, 1])
-                modified_formula.length += 1
-                modified_formula_new.variables[var] = -1 * modified_formula.variables[var]
-                modified_formula_new.length += 1
-                formulas.append(modified_formula_new)
-            self.formulas = formulas
-
-
-
-    def fill(self, satisfiable, max_len, max_variable_number):
-        # drawing whether the set of formulas will be satisfiable
+            
+    def fill(self, max_len, max_variable_number):
+        self.clear()
+        satisfiable = randint(0, 100)
+        print(satisfiable) # drawing whether the set of formulas will be satisfiable
         #generating safisfiable sets
-        if satisfiable: 
+        if satisfiable <= CHANCE_FOR_SATISFIABLE: 
+            self.satisfiable = True
             var_used = [[0,0] for i in range(max_variable_number)] # keeps track of whether value of certain variable has been generated
             # generating formulas
             for i in range(len(self.valuation)): # drawing value for each variable
@@ -142,9 +90,8 @@ class Generator:
             for formula in self.formulas:
                 # draw a variable which provides satisfiabilty
                 sat_var = randint(0, formula.size - 1)
-                if False in used_to_satisfy:
-                    while used_to_satisfy[sat_var] == True:
-                        sat_var = randint(0, formula.size - 1)
+                while used_to_satisfy[sat_var] == True:
+                    sat_var = randint(0, formula.size - 1)
 
                 initialized = [False for i in range(max_variable_number)]
                 initialized[sat_var] = True
@@ -174,57 +121,59 @@ class Generator:
             # at the end Set_Of_Formulas.formulas stores the set of formulas which can be satisfied by the Set_Of_Formulas.valuation. Keep in mind that .valuation is not the only correct valuation and the .valuation[i] has value 0 if i'th variable may either be True or False
             return
         else: # creating contradictory sets
+            self.satisfiable = False
             vars = [i for i in range(max_variable_number)]
             formulas = []
             formulas.append(Formula(max_variable_number))
             while len(vars) > 0:
-                if len(formulas) == self.size:
-                    break
                 modified_formula = choice(formulas)
                 if modified_formula.length == max_len:
                     continue
                 var = choice(vars)
                 vars.remove(var)
-                backlog = randint(0, max(0, modified_formula.length // 2 - max_variable_number // 2))
-                modified_formula_new = modified_formula.copy()
-                if (modified_formula.length >= 0):
-                    initialized = []
-                    for formula in formulas:
-                        print(formula.variables)
-                    cnts = [1 for i in range(max_variable_number)]
-                    for i in range(max_variable_number):
-                        if modified_formula.variables[i] != 0:
-                            for formula in formulas:
-                                if formula.variables[i] != 0:
-                                    cnts[i] *= 2
-
-                    for c in range(len(cnts)):
-                        if cnts[c] > 1:
-                            for i in range(cnts[c]):
-                                initialized.append(c)
-
-                    modified_formulas = [modified_formula_new, modified_formula]
-                    while backlog > 0 or len(initialized) != 0:
-                        backlog -= 1
+                backlogs = [randint(0, max(0, modified_formula.length // 2 - 1)), randint(0, max(0, modified_formula.length // 2 - 1))]
+                modified_formula_cut = modified_formula.copy()
+                initialized = []
+                for i in range(modified_formula.size):
+                    if modified_formula.variables[i] != 0:
+                        initialized.append(i)
+                modified_formulas = [modified_formula.copy(), modified_formula]
+                for i in range(len(backlogs)):
+                    while backlogs[i] > 0:
+                        backlogs[i] -= 1
                         id = choice(initialized)
-                        for i in range(initialized.count(id)):
-                            initialized.remove(id)
-                        which_one = choice([0, 1])
-                        modified_formulas[which_one].variables[id] = 0
-                        modified_formulas[which_one].length -= 1
-
+                        initialized.remove(id)
+                        modified_formulas[i].variables[id] = 0
+                        modified_formula[i].length -= 1
                 modified_formula.variables[var] = choice([-1, 1])
                 modified_formula.length += 1
-                modified_formula_new.variables[var] = -1 * modified_formula.variables[var]
-                modified_formula_new.length += 1
-                formulas.append(modified_formula_new)
+                modified_formula_cut.variables[var] = -1 * modified_formula.variables[var]
+                modified_formula_cut.length += 1
+                formulas.append(modified_formula_cut)
             self.formulas = formulas
 
+        # returns a list of lists according to a specification given at the bottom of this document
+        def get_list_of_lists(self):
+            list_of_lists = []
+            for formula in self.formulas:
+                list_of_lists.append([formula.length, formula.variables])
+            return list_of_lists
 
 
-def good_generate(difficulty):
-    abc = Generator(difficulty)   
 
+
+"""DEMO"""
+
+
+def generate(max_variable_number, formulas_number, max_len):
+    formulas = Generator(formulas_number, max_variable_number)
+    formulas.fill(max_len, max_variable_number)
+    return formulas.get_list_of_lists()
+
+def good_generate():
+    abc = Generator(formulas_number, max_variable_number)   
+    abc.fill(max_len, max_variable_number)        
+            
     print(f"Size of set:{abc.size}\nSatisfiable?: {abc.satisfiable}\nExample of correct valuation:\n{abc.valuation}\nFormulas:")
     for formula in abc.formulas:
         print(f"Formula: {formula.variables}, Length: {formula.length}")

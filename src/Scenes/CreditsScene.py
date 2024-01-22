7caebb574ace35857d9929f872ed766dbd9b1c0c
szaranczuk <igor.hanczaruk@gmail.com>
2024-01-22 04:08:59 +0100

    Added igor hanczaruk in credits

--- src/Scenes/CreditsScene.py
+++ src/Scenes/CreditsScene.py
@@ -16,21 +16,25 @@ class Credits_Text:
 class CreditsScene(BaseScene):
     def __init__(self, display, gameStateManager, background_color=(52, 26, 0)):
         BaseScene.__init__(self, display=display, gameStateManager=gameStateManager, background_color=background_color)
-        self.section_offset = 0.2*self.height
-        self.interline = 0.1 * self.height
+        self.section_offset = 0.35*self.height
+        self.interline = 0.2 * self.height
         self.top_margin = 0.1 * self.height
-        self.left_margin = 0.2 * self.width
+        self.left_margin = 0.05 * self.width
         self.init_y_offset = self.height
         self.animation_speed = 0.001*self.height
-        self.start_screen_button = setup_button(self.gameStateManager, 'start', (100, 300))
+        self.start_screen_button = setup_button(self.gameStateManager, 'start', (-0.1*self.width, 0.9*self.height))
         self.add_ui_element(self.start_screen_button)
 
         self.sections = []
-        self.logo_section = [Credits_Text(os.path.join(ASSETS_DIR, "piekielna_rezolucja_gif"), 0.1 * self.width)]
+        credits_dir = os.path.join(ASSETS_DIR, "credits")
+        authors_dir = os.path.join(credits_dir, "authors")
+        self.logo_section = [Credits_Text(os.path.join(credits_dir, "game_title"), 0.0 * self.width)]
         self.sections.append(self.logo_section)
         self.authors_section = []
-        self.authors_title = Credits_Text(os.path.join(ASSETS_DIR, "credits", "authors", "author_title"), 0.12 * self.width)
+        self.authors_title = Credits_Text(os.path.join(authors_dir, "author_title"), 0.05 * self.width)
         self.authors_section.append(self.authors_title)
+        self.igor_hanczaruk = Credits_Text(os.path.join(authors_dir, "igorhanczaruk"), 0.18 * self.width)
+        self.authors_section.append(self.igor_hanczaruk)
 
         self.sections.append(self.authors_section)
 
@@ -47,7 +51,6 @@ class CreditsScene(BaseScene):
                 element.pos_rect[1] -= self.animation_speed
                 if (element.pos_rect[1] <= 0):
                     element.pos_rect[1] -= 2*self.animation_speed
-                print(element.pos_rect)
         super().update(mouse)
 
     def on_entry(self, *args, **kwargs):

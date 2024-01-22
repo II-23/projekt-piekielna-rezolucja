315ff1c19a9590d903018a09ff71c57e17f9f626
Krzychuo <krzychuo93@gmail.com>
2024-01-22 23:14:47 +0100

    Added support for multiple amogi colors

--- src/Utils/Enemy.py
+++ src/Utils/Enemy.py
@@ -2,7 +2,8 @@ import pygame
 from Utils.Area import Area
 from Utils.ImageButton import ImageButton
 from Config.definitnios import ASSETS_DIR
-import os 
+import os
+from random import randint
 
 class Enemy:
     def __init__(self, position, size, on_enter_event, image_dir, dead_dir) -> None:
@@ -12,8 +13,18 @@ class Enemy:
         self.health = 1
         self.alive = True
         self.on_enter_event = on_enter_event
-        image_path = os.path.join(ASSETS_DIR, image_dir)
-        dead_image_path = os.path.join(ASSETS_DIR, dead_dir)
+        if image_dir == 'ghost.png':
+            roll = randint(1, 1000)
+            if roll != 1001:
+                rollcolor = randint(1, 7)
+                image_path = os.path.join(ASSETS_DIR, 'amogi', f"ghost{rollcolor}.png")
+                dead_image_path = os.path.join(ASSETS_DIR, 'amogi', f"ghost{rollcolor}_dead.png")
+            else:
+                image_path = os.path.join(ASSETS_DIR, 'amogi', f"amogus_gif.gif")
+                dead_image_path = os.path.join(ASSETS_DIR, 'amogi', f"ghost{rollcolor}_dead.png")
+        else:
+            image_path = os.path.join(ASSETS_DIR, image_dir)
+            dead_image_path = os.path.join(ASSETS_DIR, dead_image_path)
         self.sprite = ImageButton(position, size, image_path, image_path, None)     
         self.dead_sprite = ImageButton(position, size, dead_image_path, dead_image_path, None)        
 

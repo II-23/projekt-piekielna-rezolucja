fd31847d0ac492fe6f2307a28b4f78150f2f5cf9
o0something <just44d4@gmail.com>
2024-01-22 23:40:53 +0100

    moved points to be farthest left

--- src/Utils/Health_and_points.py
+++ src/Utils/Health_and_points.py
@@ -26,7 +26,7 @@ class Health_and_points:
         if self.difficulty is None and self.enemies_alive is None:
             self.text = f'Health: {self.health} Points: {self.points}'
         else:
-            self.text = f'Health: {self.health} Points: {self.points} Difficulty: {self.difficulty} Enemis left:{self.enemies_alive}'
+            self.text = f'Health: {self.health} Difficulty: {self.difficulty} Enemis left: {self.enemies_alive} Points: {self.points}'
 
     def render(self,screen):
         pygame.draw.rect(screen, self.col_bar, self.button_rect)
